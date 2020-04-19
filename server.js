c6ae78de4269385e55e79d96ab37a4921023d857
JurajZitnansky <zitnansky8@uniba.sk>
2020-04-19 17:59:42 +0200

    updates to deploy to heroku - move server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/tia-projekt'));
+app.use(express.static(__dirname + '/dist/TIA-projekt'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/tia-projekt/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/TIA-projekt/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
